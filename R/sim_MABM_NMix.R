7133a54016f979cbc7120ea2ae55164c63c94f7c
Adam Smith <avianmigration@gmail.com>
2018-07-13 14:01:13 -0400

    Incorporate correlation in random effect intercept and slope; should close #1
    As with `sim_MABM_C`, overhaul of how data set it created to make it more transparent...

--- R/sim_MABM_NMix.R
+++ R/sim_MABM_NMix.R
@@ -24,94 +24,79 @@ sim_MABM_NMix <- function(n_sites = 50,          # number of routes, assumed ind
                           obs_cov_b = 0,         # coefficient (link scale) of scaled observation-level covariate
                           sigma_site = 0.5,      # random intercept SD across sites in log(baseline bats DETECTED)
                           sigma_r_site = 0.15,   # random slope SD across sites in log(annual rate of change)  
-                          sigma_yr = 0.15,       # random intercept SD across years in log(baseline bats DETECTED)
-                          store_meta = FALSE) {  # Keep simulation parameters as metadata?
-  
+                          sigma_site_cor = -0.2, # correlation between random intercept and slope
+                          sigma_yr = 0.15) {     # random intercept SD across years in log(baseline bats DETECTED)
+
   # Calculate baseline N (actual abund) from C (detected) and detection probability
   base_N <- base_C / base_p
 
-  # Variable indicating years from initial year
-  year_cov <- rep(seq_len(n_years) - 1, each = n_visits)
+  ### Year-related effects
+  year_cov <- seq(n_years) - 1
   
   # Filter data to years surveyed
   if (survey_interval > 1) {
-    keep_yrs <- seq(1, n_years, by = survey_interval)
-    year_cov <- year_cov[year_cov %in% (keep_yrs - 1)]
+    keep_yrs <- seq(0, n_years - 1, by = survey_interval)
+    year_cov <- year_cov[year_cov %in% keep_yrs]
   }
   n_yrs_surv <- n_distinct(year_cov)
-
-  # Site-level relative abundance covariate...
-  site_cov <- as.numeric(scale(rep(rnorm(n_sites), each = n_yrs_surv * n_visits)))
-
-  ### THIS WILL BE INCORPORATED INTO EFFECT ON P BELOW
-  # Observation-level (e.g., detection) covariate...
-  # We try to make it look like a sequential variable (e.g., day of year)
-  obs_cov <- as.vector(replicate(n_sites * n_yrs_surv, sort(runif(n_visits, -30, 30))))
-  obs_cov <- obs_cov_v <- as.numeric(scale(obs_cov))
-  obs_cov <- matrix(obs_cov, ncol = 2, byrow = TRUE)
-  obs_cov <- aperm(`dim<-`(t(obs_cov), list(n_visits, n_sites, n_yrs_surv)), c(2,1,3))
-
-  # Random site intercept and annual change slope
-  # Assumed independent
-  site_eta <- rnorm(n_sites, 0, sigma_site)
-  site_eta <- rep(as.numeric(scale(site_eta, scale = FALSE)), 
-                  each = n_yrs_surv * n_visits)
-  site_r_eta <- rnorm(n_sites, 0, sigma_r_site)
-  site_r_eta <- rep(as.numeric(scale(site_r_eta, scale = FALSE)), 
-                    each = n_yrs_surv * n_visits)
+  
+  # Random intercept for each year
   yr_eta <- rnorm(n_yrs_surv, 0, sigma_yr)
-  yr_eta <- as.numeric(scale(rep(rep(yr_eta, each = n_visits), n_sites), scale = FALSE))
+  year_df <- data.frame(year = year_cov, yr_eta)
+  ###
+
+  ### With years to be surveyed decided, create base data.frame
+  dat <- expand.grid(visit = seq(n_visits),
+                     site = seq(n_sites),
+                     year = year_cov)
   
-  # Calculate site x year "actual" abundances
-  log_lam <- log(base_N) + site_eta + yr_eta +   # Intercepts
-    site_cov_b * site_cov +                      # Site-level covariate
-    log(1 + delta_r) * year_cov +                # Annual rate of change           
-    site_r_eta * year_cov                        # Random slope in annual rate of change
+  ### Site-level effects
+  # Site-level relative abundance covariate...
+  site_cov <- runif(n_sites)
+  
+  # Potentially correlated random site intercept and annual change slope
+  re_covar <- sigma_site_cor * sigma_site * sigma_r_site
+  site_vcov <- matrix(c(sigma_site^2, rep(re_covar, 2), sigma_r_site^2), 2, 2)
+  site_re <- MASS::mvrnorm(n_sites, mu = c(0,0), Sigma = site_vcov)
+  
+  site_df <- data.frame(site = seq(n_sites), site_cov,
+                        site_eta = site_re[, 1], site_r_eta = site_re[, 2])
+  ###
+
+  ### Observation-level (e.g., detection) effects
+  # Make it appear as a sequential variable (e.g., day of year)
+  obs_cov <- as.vector(replicate(n_sites * n_yrs_surv, sort(runif(n_visits))))
+  ###
+
+  # Paste it all together, scale relevant variables, and calculate linear predictor
+  # of for (latent) actual abundance...
+  scale <- function(x) (x - mean(x, na.rm = TRUE)) / sd(x, na.rm = TRUE)
+  dat <- left_join(dat, site_df, by = "site") %>%
+    left_join(year_df, by = "year") %>%
+    cbind(obs_cov) %>%
+    mutate_at(vars(c("site_cov", "obs_cov")), scale) %>%
+    mutate(lp_N = log(base_N) + site_eta + yr_eta + # Intercepts
+             site_cov_b * site_cov +              # Site-level covariate
+             log(1 + delta_r) * year +           # Annual rate of change
+             site_r_eta * year)                  # Random slope in annual rate of change
+  
+  # Add relative abundances
   h <- rgamma(n = n_sites * n_yrs_surv, shape = theta, rate = theta)
-  N_P <- rpois(n = n_sites * n_yrs_surv, lambda = exp(log_lam))
-  N_NB <- rpois(n = n_sites * n_yrs_surv, lambda = h * exp(log_lam))
-  if (!negbin) N_vec <- N_P else N_vec <- N_NB
+  N_P <- rpois(n = n_sites * n_yrs_surv * n_visits, lambda = exp(dat$lp_N))
+  N_NB <- rpois(n = n_sites * n_yrs_surv * n_visits, lambda = h * exp(dat$lp_N))
+  if (!negbin) N <- N_P else N <- N_NB
+  dat$N <- N
 
-  # Populate actual abundance array
-  N <- array(N_vec, dim = c(n_sites, n_yrs_surv))
+  # Calculate detection probability from the observation model,
+  # and simulate observed count of individuals
+  dat <- mutate(dat,
+                p = plogis(qlogis(base_p) + obs_cov_b * obs_cov)) %>%
+    rowwise() %>%
+    mutate(C = rbinom(1, N, p)) %>%
+    ungroup()
 
-  # Populate the count array and the logit detection probability array
-  C <- array(NA_integer_, dim = c(n_sites, n_visits, n_yrs_surv))
-  lp <- qlogis(base_p) + obs_cov_b * obs_cov 
-
-  for (i in 1:n_sites){
-    for (k in 1:n_yrs_surv){
-      # If assuming constant p, could have estimated all j without loop
-      # C[i, ,k] <- rbinom(n_visits, size = N[i,k], prob = base_p)
-      for (j in 1:n_visits){ 
-        C[i,j,k] <- rbinom(1, size = N[i,k], prob = plogis(lp[i,j,k]))
-  }}}
-
-  if (store_meta)
-    meta <- data.frame(variable = c("n_sites", "n_visits", "n_years",
-                                    "survey_interval", "n_years_w_surveys",
-                                    "delta_r", "base_C", "base_p", "negbin", "theta",
-                                    "sigma_site", "sigma_yr", "sigma_r_site"),
-                       value = c(n_sites, n_visits, n_years,
-                                 survey_interval = paste("every", survey_interval, "years"),
-                                 n_yrs_surv,
-                                 paste0(round(delta_r * 100, 3), "%"),
-                                 base_C, base_p, negbin, 
-                                 ifelse(negbin, theta, NA_character_),
-                                 sigma_site, sigma_yr, sigma_r_site),
-                       stringsAsFactors = FALSE)
-
-  dat <- data.frame(C = as.vector(C),
-                    visit = 1:n_visits,
-                    year = year_cov,
-                    yf = as.factor(year_cov),
-                    site = rep(1:n_sites, each = n_yrs_surv * n_visits),
-                    site_cov = site_cov,
-                    obs_cov = obs_cov_v) %>%
+  dat <- dat %>%
+    select(site, year, visit, N, p, C, site_cov, obs_cov) %>%
     arrange(site, year, visit)
-  
-  if (!store_meta)
-    return(dat)
-  else
-    return(list(meta = meta, data = dat))
+  dat
 }
